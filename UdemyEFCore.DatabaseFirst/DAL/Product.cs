3b15dee07f0c0d0486ad380575f3ab4f00c6c728
Ismail Aydemir <theangelo112@gmail.com>
2023-07-07 18:29:34 +0300

    Initial commit

---
+++ UdemyEFCore.DatabaseFirst/DAL/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyEFCore.DatabaseFirst.DAL
+{
+    public class Product // product entity'im veritabanında Product adında tabloya karşılık geliyor.
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
