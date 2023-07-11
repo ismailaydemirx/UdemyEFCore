24b8603f6a0170e31a37c17efee7e87d27d2444f
Ismail Aydemir <theangelo112@gmail.com>
2023-07-11 19:08:42 +0300

    DbSet Methods Finished

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -8,23 +8,22 @@ Initializer.Build();
 
 using (var _context = new AppDbContext()) // using kullanmamızın sebebi işlemimiz bittiği zaman bu new'leme yaptığımız işlem memory'den dispose olsun yani silinsi ki boş yer kaplamasın.
 {
+    var product = await _context.Products.FirstAsync(x => x.Id == 3);
+    product.Name = "ismail";
+    Console.WriteLine(_context.Entry(product).State);
+    //products.ForEach(p =>
+    //{
 
-    _context.Products.Add(new() {Name="Kalem 1",Price=200,Stock=100,Barcode=123});
-    _context.Products.Add(new() {Name="Kalem 1",Price=200,Stock=100,Barcode=123});
-    _context.Products.Add(new() {Name="Kalem 1",Price=200,Stock=100,Barcode=123});
-
-
-    Console.WriteLine($"Context Id? {_context.ContextId}");
-
-   // _context.SaveChanges();
-
-
+    //    var state = _context.Entry(p).State;
+    //    Console.WriteLine($"{p.Id} : {p.Name} - {p.Price} - {p.Stock} state: {state}");
+    //});
 
 
+    //Console.WriteLine($"Context Id? {_context.ContextId}");
 
+    // _context.SaveChanges();
     //_context.Update(new Product() { Id=5,Name="Defter",Price=150,Stock=100,Barcode=425});
     //_context.Entry(product).State = EntityState.Deleted; üstteki ile aynı.
-
 
     //await _context.SaveChangesAsync();
     // Console.WriteLine("save change state: " + _context.Entry(product).State);
@@ -35,14 +34,6 @@ using (var _context = new AppDbContext()) // using kullanmamızın sebebi işlem
 
 
     //var products = await _context.Products.AsNoTracking().ToListAsync();//git veritabanıyla haberleş demek - ToListAsync asenkron olduğundan başına 'await' keyword'ü ekledik.
-
-    //products.ForEach(p =>
-    //{
-    //    p.Stock += 100;
-    //    var state = _context.Entry(p).State;
-    //    //Console.WriteLine($"{p.Id} : {p.Name} - {p.Price} - {p.Stock} state: {state}");
-    //});
-
 
 
 }
