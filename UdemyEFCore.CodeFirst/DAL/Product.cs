9d7d13c72418289c7f5b8a7b0f5980dfc3be4505
Ismail Aydemir <theangelo112@gmail.com>
2023-07-12 18:53:31 +0300

    Relationships One-to-Many

--- UdemyEFCore.CodeFirst/DAL/Product.cs
+++ UdemyEFCore.CodeFirst/DAL/Product.cs
@@ -16,9 +16,7 @@ namespace UdemyEFCore.CodeFirst.DAL
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int Barcode { get; set; }
-        public int Category_Id { get; set; } // Category şu anda parent olarak farzediyoruz EF Core burada belirttiğimiz CategoryId 'yi foreign key olarak algılıyor. Id yazısının önündeki ek olan Category ekinden bu durumu anlıyor.
-        // Navigation Property
-        
-        public Category Category { get; set; } // Burada Category veritabanında oluşmayacak, bunlar EF Core'da Product'dan Category'e, Category'den Product'a geçmek için kullandığımız navigation property'lerdir.
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
