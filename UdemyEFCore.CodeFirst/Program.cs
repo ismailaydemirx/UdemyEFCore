d4f82748bf8d50b9d2a0e4a9cbada083c8d35d85
Ismail Aydemir <theangelo112@gmail.com>
2023-07-31 15:21:35 +0300

    Query | Left/Right Join

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -9,45 +9,34 @@ Initializer.Build();
 
 using (var _context = new AppDbContext()) // using kullanmamızın sebebi işlemimiz bittiği zaman bu new'leme yaptığımız işlem memory'den dispose olsun yani silinsi ki boş yer kaplamasın.
 {
-    // JOIN YAPISINI KULLANALIM. LINQ şeklinde ilerleyelim.
 
-    // 2'li Join
-    //var result = _context.Categories.Join(_context.Products, x => x.Id, y => y.CategoryId, (c, p) => p).ToList();
-
-    // 2. YOL 2'Lİ JOIN SQL Cümleciği Gibi Yazabiliriz.
-
-    //var result2 = (from c in _context.Categories
-    //               join p in _context.Products on c.Id equals p.CategoryId
-    //               select new
-    //               {
-    //                   CategoryName = c.Name,
-    //                   ProductName = p.Name,
-    //                   ProductPrice = p.Price,
-    //               }).ToList();
-
-
-    // 3'lü Join
-    var result = _context.Categories
-        .Join(_context.Products, x => x.Id, y => y.CategoryId, (category, product) => new { category, product })
-        .Join(_context.ProductFeature, x => x.product.Id, y => y.Id, (category, productFeature) => new
-        {
-            CategoryName = category.category.Name,
-            ProductName = category.product.Name,
-            ProductFeatureColor = productFeature.Color,
-        }).ToList();
-
-
-    // 2. YOL 3'LÜ JOIN SQL Cümleciği Gibi Yazabiliriz.
-
-    var result2 = (from category in _context.Categories
-                   join product in _context.Products on category.Id equals product.CategoryId
-                   join productFeature in _context.ProductFeature on product.Id equals productFeature.Id
-                   select new { category, product, productFeature }).ToList();
-
+    // RIGHT JOIN -- Right join'i left join kullanarak gerçekleştiriyoruz. Tabloların yerini değiştirince aynı şeye denk geliyor.
+    var rightJoinResult = await (from pf in _context.ProductFeature
+                                 join p in _context.Products on pf.Id equals p.Id into plist
+                                 from p in plist.DefaultIfEmpty()
+                                 select new
+                                 {
+                                     ProductName = p.Name,
+                                     ProductPrice = (decimal?)p.Price,
+                                     ProductColor = pf.Color,
+                                     ProductWidth = pf.Width,
+                                 }).ToListAsync();
 
 
 
     Console.WriteLine("");
+
+
+    // LEFT JOIN 
+    var leftJoinResult = await (from p in _context.Products
+                                join pf in _context.ProductFeature on p.Id equals pf.Id into pflist
+                                from pf in pflist.DefaultIfEmpty()
+                                select new
+                                {
+                                    ProductName = p.Name,
+                                    ProductColor = pf.Color,
+                                    ProductWidth = (int?)pf.Width == null ? 5 : pf.Width,
+                                }).ToListAsync();
 
     //var category = new Category() { Name = "Kalemler" };
     //category.Products.Add(new() { Name = "kalem 1", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
