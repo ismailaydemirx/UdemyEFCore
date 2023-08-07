7199e98d310104573d9a2371775959c6210f5f98
Ismail Aydemir <theangelo112@gmail.com>
2023-08-07 16:51:07 +0300

    Isolation Level | Repeatable Read

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -14,18 +14,17 @@ using UdemyEFCore.CodeFirst.Mappers;
 
 using (var context = new AppDbContext())
 {
-    using (var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.ReadCommitted))
+    using (var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead))
     {
         // Isolation Levels | Read Committed
 
         //var products = context.Products.ToList();
 
+        var product = context.Products.Take(2).ToList();
+        //product.Price = 3000;
 
-        var product = context.Products.First();
-        product.Price = 3000;
-
-        context.Products.Add(new Product() { Name = "a", Price = 1, Barcode = 2, CategoryId = 1, DiscountPrice = 1 });
-        context.SaveChanges();
+        //context.Products.Add(new Product() { Name = "a", Price = 1, Barcode = 2, CategoryId = 1, DiscountPrice = 1 });
+        //context.SaveChanges();
 
         transaction.Commit();
 
