b9738326b6dd1a09d3cf57b20869f78d59baa1ef
Ismail Aydemir <theangelo112@gmail.com>
2023-08-08 15:15:11 +0300

    Isolation Level | Serilizable

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -14,13 +14,16 @@ using UdemyEFCore.CodeFirst.Mappers;
 
 using (var context = new AppDbContext())
 {
-    using (var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead))
+    using (var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable))
     {
-        // Isolation Levels | Read Committed
+        // Isolation Levels | Serializable
 
         //var products = context.Products.ToList();
 
-        var product = context.Products.Take(2).ToList();
+        // Burada ReeatableRead ile en üstteki iki veriyi take ile aldık artık o verileri ne silebiliriz ne de güncelleyebiliriz.
+
+        var product = context.Products.ToList();
+
         //product.Price = 3000;
 
         //context.Products.Add(new Product() { Name = "a", Price = 1, Barcode = 2, CategoryId = 1, DiscountPrice = 1 });
