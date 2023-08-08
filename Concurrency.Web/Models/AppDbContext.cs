68d2bf48e79225dd631342314fce9e023875dbd2
Ismail Aydemir <theangelo112@gmail.com>
2023-08-08 16:41:39 +0300

    Concurrency | Asp.Net Core MVC Projesi Oluşturmak

---
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API tanımladığımız yer: (Best Practice olan her zaman Fluent API kullanmaktır.)
+            modelBuilder.Entity<Product>().Property(x => x.RowVersion).IsRowVersion();
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2); // decimal alanlarında virgülden sonra kaç hane olacağını belirtmemiz gerekiyor. Yoksa hata verir.
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Concurrency.Web/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concurrency.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        // [Timestamp] // 1. yol Zaman damgası
+        public byte[] RowVersion { get; set; }
+    }
+}
