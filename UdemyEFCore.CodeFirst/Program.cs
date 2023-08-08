200691d04b7dff85e9c228d8604369d8b5e89792
Ismail Aydemir <theangelo112@gmail.com>
2023-08-08 15:30:59 +0300

    Isolation Levels | Snapshot

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -14,20 +14,16 @@ using UdemyEFCore.CodeFirst.Mappers;
 
 using (var context = new AppDbContext())
 {
-    using (var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable))
+    using (var transaction = context.Database.BeginTransaction(System.Data.IsolationLevel.Snapshot))
     {
-        // Isolation Levels | Serializable
+        // Isolation Levels | Snapshot
 
-        //var products = context.Products.ToList();
+        var products = context.Products.AsNoTracking().ToList();
 
-        // Burada ReeatableRead ile en üstteki iki veriyi take ile aldık artık o verileri ne silebiliriz ne de güncelleyebiliriz.
+        // business code
 
-        var product = context.Products.ToList();
+        var product2 = context.Products.ToList();
 
-        //product.Price = 3000;
-
-        //context.Products.Add(new Product() { Name = "a", Price = 1, Barcode = 2, CategoryId = 1, DiscountPrice = 1 });
-        //context.SaveChanges();
 
         transaction.Commit();
 
