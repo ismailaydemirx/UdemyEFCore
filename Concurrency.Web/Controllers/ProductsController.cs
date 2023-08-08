a3af7df251c848db46354a3b050b251aa4a97c11
Ismail Aydemir <theangelo112@gmail.com>
2023-08-08 20:00:48 +0300

    Concurrency | DbUpdateConcurrencyException Exception

--- Concurrency.Web/Controllers/ProductsController.cs
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Concurrency.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Concurrency.Web.Controllers
 {
@@ -29,10 +30,40 @@ namespace Concurrency.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(Product product)
         {
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Products.Update(product);
+                await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(List));
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var exceptionEntry = exception.Entries.First();
+
+                var currentProduct = exceptionEntry.Entity as Product;
+                
+                var databaseValues = exceptionEntry.GetDatabaseValues();
+                var clientValues = exceptionEntry.CurrentValues;
+
+                
+
+
+                if (databaseValues == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından silindi");
+                }
+                else
+                {
+                    var databaseProduct = databaseValues.ToObject() as Product;
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından güncellendi");
+                    ModelState.AddModelError(string.Empty, $"Güncellenen Değer: Name: {databaseProduct.Name}, Price: {databaseProduct.Price}, Stock: {databaseProduct.Stock} ");
+
+                }
+
+                return View(product);
+            }
+           
         }
 
 
